3b6520660661ad28cb5bc25a5116bda9dc0c1996
Yuyuan Kang <36235611+yuyuankang@users.noreply.github.com>
2024-11-05 16:15:02 -0600

    visible for testing

--- app/src/main/java/com/cs407/lab7/MainActivity.kt
+++ app/src/main/java/com/cs407/lab7/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.annotation.VisibleForTesting
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
